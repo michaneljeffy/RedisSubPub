ba32552870e447fa77d532a6bd8b21668db2853d
zhangshuyu <zhangshuyu@yuantutech.com>
2018-03-06 15:31:50 +0800

    redis 发布订阅

---
+++ RedisPub/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisPub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1:6379"))
+            {
+                ISubscriber sub = redis.GetSubscriber();
+                Console.WriteLine("请输入任意字符，输入exit退出");
+                string input;
+                do
+                {
+                    input = Console.ReadLine();
+                    sub.Publish("messages", input);
+                } while (input != "exit");
+            }
+        }
+    }
+}
---
+++ RedisSub/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1:6379"))
+            {
+                ISubscriber sub = redis.GetSubscriber();//获取订阅器
+                sub.Subscribe("messages", (channel, message) =>
+                {
+                    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+                });
+            }
+            Console.WriteLine("subscrible message success");
+            Console.ReadKey();
+               
+        }
+    }
+}
